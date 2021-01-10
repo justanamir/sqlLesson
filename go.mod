f810851776d2a3ca78c2d8074685a657d16526ee
justanamir <amirdanialmuslim@gmail.com>
2021-01-10 16:05:58 +0800

    connected to mariadb mysql (used docker port 3307)

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module sqlLesson
 
 go 1.13
 
-require github.com/mattn/go-sqlite3 v2.0.3+incompatible
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible
+)
--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
-	_ "github.com/mattn/go-sqlite3"
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	db, err := sql.Open("mysql", "root:5227@tcp(127.0.0.1:3307)/test")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
+	defer db.Close()
+
+	db.Exec("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, username TEXT, email TEXT, password TEXT)")
+
+	insert, err := db.Query("INSERT INTO user (username, email, password) VALUES (?, ?, ?)", "jun", "jun@g.com", "1234")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer insert.Close()
+
+	rows, err := db.Query("SELECT	* FROM user")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var id int
+	var username string
+	var email string
+	var password string
+
+	for rows.Next() {
+		rows.Scan(&id, &username, &email, &password)
+		fmt.Println(id, username, email, password)
+	}
 }
